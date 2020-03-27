cbe231599e4ab49d3a417e84ef7a46fef000378c
福岡和樹 <kazuki@mba.local>
2020-03-27 13:49:54 +0900

    study_management

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_142824) do
+ActiveRecord::Schema.define(version: 2020_03_27_042228) do
 
   create_table "graphs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "date"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_03_26_142824) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id", "date", "study_time", "score", "subject"], name: "user_index", unique: true
+    t.index ["user_id", "date"], name: "index_graphs_on_user_id_and_date", unique: true
     t.index ["user_id"], name: "index_graphs_on_user_id"
   end
 
