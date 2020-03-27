954c8ccbf1fb8dcbfcf2cfe84f3547ca8d19b610
福岡和樹 <kazuki@mba.local>
2020-03-27 11:16:07 +0900

    study_management

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -2,6 +2,41 @@ document.addEventListener('turbolinks:load', () => {
   // '2020-01-12'のような文字列から，Javascriptの日付オブジェクトを取得する関数
   // setHoursを使用しないと，時差の影響で0時にならないため注意！
   const convertDate = (date) => new Date(new Date(date).setHours(0, 0, 0, 0))
+  // 日付の古い方・新しい方を取得する関数
+  const minDate = (date1, date2) => (date1 < date2) ? date1 : date2
+  const maxDate = (date1, date2) => (date1 > date2) ? date1 : date2
+
+  // データの初日・最終日
+  const START_DATE = convertDate(gon.user_records[0].date)
+  const END_DATE = convertDate(gon.user_records[gon.user_records.length - 1].date)
+
+  // カレンダーの日本語化
+  flatpickr.localize(flatpickr.l10ns.ja)
+
+  const drawGraphForPeriod = () => {
+    let from = convertDate(document.getElementById('start-calendar').value)
+    let to = convertDate(document.getElementById('end-calendar').value)
+
+    if (from > to) {
+        alert('終了日は開始日以降の日付に設定して下さい')
+    } else {
+        drawGraph(from, to)
+    }
+  }
+
+  const periodCalendarOption = {
+      // スマートフォンでもカレンダーに「flatpickr」を使用
+      disableMobile: true,
+      // 選択できる期間を設定
+      minDate: START_DATE,
+      maxDate: END_DATE,
+      // 日付選択後のイベント
+      onChange: drawGraphForPeriod
+  }
+
+  // カレンダー
+  const startCalendarFlatpickr = flatpickr('#start-calendar', periodCalendarOption)
+  const endCalendarFlatpickr = flatpickr('#end-calendar', periodCalendarOption)
 
   const TODAY = convertDate(new Date())
   const A_WEEK_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 6)
@@ -11,6 +46,10 @@ document.addEventListener('turbolinks:load', () => {
 
   // グラフを描く場所を取得
   const chartStudyTimeContext = document.getElementById("chart-study-time").getContext('2d')
+
+// 関数内で変数宣言をするとローカル変数となり，関数の外で消えてしまう
+  // drawGraph 関数の外で変数宣言をしなければならない!
+  let chartStudyTime
 
   // 期間を指定してグラフを描く
   const drawGraph = (from, to) => {
@@ -27,7 +66,7 @@ document.addEventListener('turbolinks:load', () => {
           return record.date.replace(/^\d+-0*(\d+)-0*(\d+)$/, '$1/$2')
       })
 
-      // 体重のみのデータを作成
+      // 学習時間のみのデータを作成
       let study_time = records.map((record) => record.study_time)
 
       let studyTimeData = {
@@ -42,7 +81,7 @@ document.addEventListener('turbolinks:load', () => {
           }]
       }
 
-      let StudyTimeOption = {
+      let studyTimeOption = {
           tooltips: {
               callbacks: {
                   // ホバー（スマホならタップ）時のラベル表示を変更
@@ -56,13 +95,48 @@ document.addEventListener('turbolinks:load', () => {
           }
       }
 
-      new Chart(chartStudyTimeContext, {
-          type: 'line',
-          data: studyTimeData,
-          options: StudyTimeOption
-      })
+      if (!chartStudyTime) {
+        // グラフが存在しないときは，作成する
+        chartStudyTime = new Chart(chartStudyTimeContext, {
+            type: 'line',
+            data: studyTimeData,
+            options: studyTimeOption
+        })
+    } else {
+        // グラフが存在するときは，更新する
+        chartStudyTime.data = studyTimeData
+        chartStudyTime.options = studyTimeOption
+        chartStudyTime.update()
+    }
   }
 
+// 引数の日付から今日までのグラフを描く関数
+    const drawGraphToToday = (from) => {
+      // データが存在する範囲に修正
+      from = maxDate(from, START_DATE)
+      let to = minDate(TODAY, END_DATE)
+      drawGraph(from, to)
+      // フォームの開始日・終了日を変更する
+      startCalendarFlatpickr.setDate(from)
+      endCalendarFlatpickr.setDate(to)
+  }
+
+  // 過去◯週間のグラフを描くボタン
+  document.getElementById('a-week-button').addEventListener('click', () => {
+      drawGraphToToday(A_WEEK_AGO)
+  })
+
+  document.getElementById('two-weeks-button').addEventListener('click', () => {
+      drawGraphToToday(TWO_WEEKS_AGO)
+  })
+
+  document.getElementById('a-month-button').addEventListener('click', () => {
+      drawGraphToToday(A_MONTH_AGO)
+  })
+
+  document.getElementById('three-months-button').addEventListener('click', () => {
+      drawGraphToToday(THREE_MONTHS_AGO)
+  })
   // グラフの初期表示
-  drawGraph(A_WEEK_AGO, TODAY)
+  drawGraphToToday(A_WEEK_AGO)
 })
